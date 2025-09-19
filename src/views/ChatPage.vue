9aa0f53a4f9da3f74ecc061e66ffd6572c3a7994
Hu Xianming <78032724@qq.com>
2025-09-19 12:44:40 +0800

    代码优化2

--- src/views/ChatPage.vue
+++ src/views/ChatPage.vue
@@ -491,7 +491,7 @@ const handleSiderToggle = () => {
       element.style.display = "block";
     } else {
       element.style.display = "none";
-      renameDialogVisible.value = true
+      renameDialogVisible.value = false
     }
   }
 
